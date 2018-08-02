625c922d068a833477699c21e9929aec5ceddaba
liuzh135 <626787193@qq.com>
2018-08-02 14:04:13 +0800

    clear .idea

--- app/src/main/java/com/wyzk/lottery/ui/LoginActivity.java
+++ app/src/main/java/com/wyzk/lottery/ui/LoginActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
